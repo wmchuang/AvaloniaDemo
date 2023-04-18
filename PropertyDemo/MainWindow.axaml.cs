687ca5374cce372cd3453d8802aa0ecdcb06110d
wangchuang <515497782@qq.com>
2023-04-18 18:01:19 +0800

    feat: propertyDemo

---
+++ PropertyDemo/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PropertyDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow ()
+        {
+            InitializeComponent();
+        }
+    }
+}
