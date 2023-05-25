d490bef9439b175015f83d88b13b2b63c98bbf92
wangchuang <515497782@qq.com>
2023-05-25 10:18:37 +0800

    fit

--- NativeControlHostDemo/Program.cs
+++ NativeControlHostDemo/Program.cs
@@ -12,6 +12,7 @@ class Program
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
 
+
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
