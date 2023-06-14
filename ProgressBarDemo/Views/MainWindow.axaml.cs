87f74949b784117fd8e98c24abde7780d6ee2895
wangchuang <515497782@qq.com>
2023-06-14 09:33:56 +0800

    feat： progressBarDemo

---
+++ ProgressBarDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProgressBarDemo.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
